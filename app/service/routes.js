84ad413efb16a7954d4f85d47392b7ba35d5deef
LINOS NCHENA <linosnchena@gmail.com>
2019-10-08 16:45:30 +0200

    changing names to name

--- app/service/routes.js
+++ app/service/routes.js
@@ -2,13 +2,13 @@
 module.exports = function(app) {
   var rusangu = require('../Controller/controller');
 
-  app.route('/business')
+  app.route('/second')
     .get(rusangu.employeeAll)
     .post(rusangu.employeeCreate);
 
-   // http://localhost:2020/business
-   
-  app.route('/business/:taskId')  
+   // http://localhost:2020/second
+
+  app.route('/second/:taskId')  
     .get(rusangu.employeeOne)
     .put(rusangu.employeeUpdate)
     .delete(rusangu.employeeDelete);
