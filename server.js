29ca7fdfe3e418c7af2049a0a144a05227294190
LINOS NCHENA M <linosnchena@gmail.com>
2020-02-04 12:29:19 +0100

    Frontage SAAS

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   port = process.env.PORT || 8081;
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(function (req, res, next) { // SECURITY
+
+app.use(function (req, res, next) {              //  SECURITY LUMUMBA
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-  });
+  next();  });
 
 var routes = require('./PAAS/service/Routes'); 
 routes(app); 
